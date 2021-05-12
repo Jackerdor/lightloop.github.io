5b0caa9d1cc3eb590d24ea383e4ca624f9cb705c
Jackerdor <jack-bill@live.com>
2021-05-12 15:55:54 +0100

    clean as a mf

---
+++ src/containers/Members.js
@@ -0,0 +1,58 @@
+function Members() {
+    return (
+      <div className="fitScreen">
+      <div className="nav-grid-space"></div>
+      <div className="team-wrapper">
+        <div className="team-flexl">
+          <div className="team-title">
+            <div className="gradient-box-team"></div>
+            THE<h1>TEAM</h1>
+            </div>
+          <div className="member-photo-wrapper">
+            <div className="member-photo">
+              <div className="member-photo-gl"></div>
+              <div className="member-photo-gb"></div>
+              <div className="member-photo-gr"></div>
+            </div>
+          </div>
+        </div>
+        <div className="team-flexr">
+          <div className="member-title">
+            <h1>JAKE NEWTON</h1>
+            <p></p>
+            LEAD 3D ARTIST
+          </div>
+          <div className="team-links">
+            <a className="website-link">VISIT WEBSITE</a>
+            <a className="orb-link"><i class="fab fa-linkedin-in"></i></a>
+            <a className="orb-link"><i class="fab fa-artstation"></i></a>
+          </div>
+          <div className="biography-header"><h1>BIOGRAPHY</h1><div className="biography-line"></div></div>
+          <div className="biography-information"> 
+          Technical problem solving within the 3D side to Visual Effect and Animation is what I enjoy most. I am a 3D generalist as I enjoy all aspects of 3D, from modeling to rendering. Along with being a 3D generalist I also work on sound design and cinematograph.<p></p> Merlin was the series which made me want to work in this industry and still to this day is one of my personal favorites. Outside of work I am an avid Mountain biker.
+          </div>
+        </div>
+        <div className="outline-box"></div>
+        <div className="team-navigator"> 
+        <a>AIDAN LONERGAN</a><sp>|</sp>
+        <a>SANDEEP TALUDHAR</a><sp>|</sp>
+        <a>JAKE NEWTON</a><sp>|</sp>
+        <a>MARTIN KAMMINGA</a>
+        <p></p>
+        <a>ALFIE MITCHELL</a><sp>|</sp>
+        <a>CHRISTOPHER FERNANDES</a><sp>|</sp>
+        <a>BILLY DANIEL ALLEN</a><sp>|</sp>
+        <a>MACIEJ OUSCH</a>
+        <p></p>
+        <a>JACK BILLING</a><sp>|</sp>
+        <a>ADAM PHILIPS</a><sp>|</sp>
+        <a>TIJAN HOLDER</a><sp>|</sp>
+        <a>ALEX ANGERT</a>
+        </div>
+      </div>
+    </div>
+
+    )
+}
+
+export default Members
---
+++ src/containers/Projects.js
@@ -0,0 +1,41 @@
+function Projects() {
+    return (
+      <div className="fitScreen">
+      <div className="nav-grid-space"></div>
+        <div className="cntrAll works-wrapper">
+          
+          <div className="cntrAll works-selected-text">SELECTED</div>
+          <div className="cntrAll carousel">
+            <div className="thumbnail2 tn-unfocused">
+              <div className="unfocused-title1">JOE</div>
+              <div className="gradient-lr"></div>
+              <div className="gradient-ll"></div>
+            </div>
+            <div className="thumbnail1 tn-focused"></div>
+            <div className="thumbnail3 tn-unfocused">
+              <div className="unfocused-title2">BLITZKRIEG</div>
+              <div className="gradient-rl"></div>
+              <div className="gradient-rr"></div>
+            </div>
+          </div>
+          <div className="cntrAll title-box">
+            <div className="title">DRAW</div>
+            <div className="directed">Directed by <p></p><h1>Martin Kamminga</h1></div>
+          </div>
+          <div className="cntrAll tag-box">
+            <div className="info-header">DESCRIPTION</div>
+            <div className="tags">
+              <div class="tag-item">SHORT FILM</div>
+              <div class="tag-item">ANIMATION</div>
+              <div class="tag-item">LIVE ACTION</div>
+            </div>
+          </div>
+          <div className="cntrAll information-box">Two kings battle for mastery of the chessboard.</div>
+          <div className="works-title"><div className="gradient-box"></div>PROJECTS</div>
+          <div className="outline-box"></div>
+        </div>
+      </div>
+    )
+}
+
+export default Projects
