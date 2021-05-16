7f658f1c4e503dacf12e6e849c52832b37e263e5
Ayoub Aabass <ayoub@aabass.net>
2021-05-16 20:34:12 +0200

    ✏ did stuff

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,11 +1,31 @@
+import React from "react";
+import { motion, useTransform } from "framer-motion";
+
 const ProjectCard = (props) => {
+  const scaleFactor = useTransform(props.boi, props.range, [1, 1.5, 1.5, 1]);
+  const zFactor = useTransform(props.boi, props.range, [1, 10, 10, 1]);
+  const marginXFactor = useTransform(props.boi, props.range, [50, -10, -10, 50]);
 
-    return (
-        <div className="thumbnail tn-unfocused" ref={props.ref} onClick={props.onClick} id={props.id} style={{backgroundImage:`url(http://localhost:3000/${props.thumbnail})`}}>
-        <div className="item-gradient-lr"></div>  
-        <div className="item-gradient-rl"></div>  
-        </div>
-    )
-}
+  console.log(props.range);
 
-export default ProjectCard
+  return (
+    <motion.div
+      className="thumbnail tn-unfocused"
+      ref={props.ref}
+      onClick={props.onClick}
+      id={props.id}
+      style={{
+        backgroundImage: `url(http://localhost:3000/${props.thumbnail})`,
+        scale: scaleFactor,
+        zIndex: zFactor,
+        marginLeft: marginXFactor,
+        marginRight: marginXFactor,
+      }}
+    >
+      <div className="item-gradient-lr"></div>
+      <div className="item-gradient-rl"></div>
+    </motion.div>
+  );
+};
+
+export default ProjectCard;
--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,108 +1,78 @@
-import ProjectCard from '../components/projects/ProjectCard'
-import projectData from '../data/project.json'
-import React, {useState, useEffect, useRef} from 'react'
-
-
+import ProjectCard from "../components/projects/ProjectCard";
+import projectData from "../data/project.json";
+import React, { useState, useEffect, useRef } from "react";
+import { useElementScroll } from "framer-motion";
 
 function Projects() {
+  const navRef = useRef();
+  const carouselRef = useRef();
+  const { scrollX } = useElementScroll(carouselRef);
+  const projectRef = [];
 
-const navRef = useRef();
-const projectRef = [];
-
-function scrollCallback(entries: IntersectionObserverEntry[]) {
-  if (entries[0].isIntersecting) {
-
-}
-}
-
-
-/* const observer = React.useRef(
-  new IntersectionObserver((entries) => {
-    const first = entries[0];
-    console.log(first);
-  }, { threshold: 1 })
-);
-const [element, setElement] = React.useState(null); 
-
-React.useEffect(() => {
-  const currentElement = element;
-  const currentObserver = observer.current
-
-  if (currentElement) {
-    currentObserver.observe(currentElement);
-  }
-
-  return () => {
-    if (currentElement){
-      currentObserver.unobserve(currentElement);
-    }
+  const clickHandler = (e) => {
+    console.log(projectRef.thumbnail);
   };
-}, [element]);
 
-let [state, setState] = useState({
-  visible: false,
-  ratio: 0
-}) */
-
-/*
-const ref = useRef()
-
-useEffect(() => { const observer = new IntersectionObserver(([entry]) => {
-  
-  setState({
-
-    visible: entry.isIntersecting,
-
-    ratio: entry.intersectionRatio
-  })
-
-}), {threshold: [.25, .5, .75, 1]
-});
-*/
-const [number, setNumber] = React.useState(0);
-
-const clickHandler = (e) => {
-  console.log(projectRef.thumbnail)
-}
-
-
-    return (
-
-      <div className="fitScreen">
+  return (
+    <div className="fitScreen">
       <div className="nav-grid-space"></div>
-        <div className="cntrAll works-wrapper">
-          
-          <div className="cntrAll works-selected-text">SELECTED</div>
-          
-          <div className="carousel-wrapper">
+      <div className="cntrAll works-wrapper">
+        <div className="cntrAll works-selected-text">SELECTED</div>
+
+        <div className="carousel-wrapper" ref={carouselRef}>
           <div className="cntrAll carousel" ref={navRef}>
-          <div className="carousel-padding"></div>
-          {projectData.map(project => <ProjectCard id={project.id} ref={index => this.elementRefs[index] = projectRef} thumbnail={project.thumbnail} onClick={clickHandler} />)}
-          <div className="carousel-padding"></div>
+            <div className="carousel-padding"></div>
+
+            {projectData.map((project, index) => (
+              <ProjectCard
+                id={project.id}
+                ref={(index) => (this.elementRefs[index] = projectRef)}
+                thumbnail={project.thumbnail}
+                onClick={clickHandler}
+                range={[
+                  600 + 500 * index - window.innerWidth / 2,
+                  800 + 500 * index - window.innerWidth / 2,
+                  1000 + 500 * index - window.innerWidth / 2,
+                  1300 + 500 * index - window.innerWidth / 2,
+                ]}
+                boi={scrollX}
+              />
+            ))}
+
+            <div className="carousel-padding"></div>
 
             <div className="gradient-rr"></div>
             <div className="gradient-ll"></div>
           </div>
-          </div>
-
-          <div className="cntrAll title-box">
-            <div className="title" onClick={clickHandler} >DRAW</div>
-            <div className="directed">Directed by <p></p><h1>Martin Kamminga</h1></div>
-          </div>
-          <div className="cntrAll tag-box">
-            <div className="info-header">DESCRIPTION</div>
-            <div className="tags">
-              <div class="tag-item">SHORT FILM</div>
-              <div class="tag-item">ANIMATION</div>
-              <div class="tag-item">LIVE ACTION</div>
-            </div>
-          </div>
-          <div className="cntrAll information-box">Two kings battle for mastery of the chessboard.</div>
-          <div className="works-title"><div className="gradient-box"></div>PROJECTS</div>
-          <div className="outline-box"></div>
         </div>
+
+        <div className="cntrAll title-box">
+          <div className="title" onClick={clickHandler}>
+            DRAW
+          </div>
+          <div className="directed">
+            Directed by <p></p>
+            <h1>Martin Kamminga</h1>
+          </div>
+        </div>
+        <div className="cntrAll tag-box">
+          <div className="info-header">DESCRIPTION</div>
+          <div className="tags">
+            <div class="tag-item">SHORT FILM</div>
+            <div class="tag-item">ANIMATION</div>
+            <div class="tag-item">LIVE ACTION</div>
+          </div>
+        </div>
+        <div className="cntrAll information-box">
+          Two kings battle for mastery of the chessboard.
+        </div>
+        <div className="works-title">
+          <div className="gradient-box"></div>PROJECTS
+        </div>
+        <div className="outline-box"></div>
       </div>
-    )
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
